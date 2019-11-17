c3f792f2a68e09615048ffb35ac20e07475b958e
DengYun <dengyun@meideng.net>
2019-11-17 22:49:55 +0800

    framework

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const webpack = require('webpack');
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = () => ({
+  context: __dirname,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  entry: {
+    index: './src',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  resolveLoader: {
+    extensions: ['.js'],
+  },
+  externals: function(context, request, callback) {
+    // node target shouldn't try to resolve built-in packages,
+    // and doesn't needs to pack with any dependencies
+    if (!/^[.\/]/.test(request) && !path.isAbsolute(request)) {
+      callback(null, 'commonjs ' + request);
+      return;
+    }
+    callback();
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          compilerOptions: {
+            target: 'esnext',
+            module: 'esnext',
+            esModuleInterop: true,
+          },
+          onlyCompileBundledFiles: true,
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          toplevel: true,
+          compress: {
+            unsafe: true,
+            passes: 3,
+          },
+        },
+      }),
+    ],
+  },
+});
